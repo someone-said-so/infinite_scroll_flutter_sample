38621eec5de8d6872149bbba116ca747fc1b1b4c
takuya.sudo <takuya.sudo@livefor.jp>
2024-07-01 19:06:35 +0900

    上方向にスクロールして配列を前方に追加する場合の追加ロードのスクロール位置調整をScrollableListの内部で行うようにする

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -25,80 +25,55 @@ class MyHomePage extends HookConsumerWidget {
     final loading = ref.watch(triviaLoadingProvider);
 
     final itemScrollController = useMemoized(() => ItemScrollController());
-    final itemPositionsListener = useMemoized(() => ItemPositionsListener.create());
     final scrollOffsetController = useMemoized(() => ScrollOffsetController());
-    final scrollOffsetListener = useMemoized(() => ScrollOffsetListener.create());
-    final listener = useMemoized(() => ScrollableListEventListener.create());
+    final scrollableListener = useMemoized(() => ScrollableListEventListener.create());
 
     final initialized = useState(false);
     final topIndex = useState(0);
     final bottomIndex = useState(0);
 
-    // スクロール位置の保存
-    final scrollIndex = useState(initialScrollIndex);
-    final leadingEdge = useState(initialLeadingEdge);
-    final scrollHistory = useState(<(double, DateTime)>[]);
-
-    ref.listen(triviaProvider, (prev, next) {
-      final scrollOffsetInSecond = scrollHistory.value
-          .where((record) => DateTime.now().difference(record.$2).inSeconds <= 1)
-          .fold(0.0, (acc, current) => acc + current.$1);
-
-      if (next.length > (prev?.length ?? 0) && initialized.value && scrollOffsetInSecond < 0.0) {
-        itemScrollController.jumpTo(index: scrollIndex.value + 10, alignment: leadingEdge.value);
-        scrollOffsetController.animateScroll(
-          offset: -500.0,
-          duration: const Duration(milliseconds: 1000),
-          curve: Curves.easeOut,
-        );
+    useEffect(() {
+      onScrollOffsetEvent() {
+        (switch (scrollableListener.event.value) {
+          ScrollableListEventScrollOffset(:final offset) => () {
+              print("offset: $offset");
+            },
+          _ => () {},
+        })();
       }
-    });
+
+      scrollableListener.event.addListener(onScrollOffsetEvent);
+      return () => scrollableListener.event.removeListener(onScrollOffsetEvent);
+    }, []);
 
     useEffect(() {
-      listener() {
-        final positions = itemPositionsListener.itemPositions.value;
-        if (positions.isNotEmpty && trivia.isNotEmpty) {
-          topIndex.value = min(positions.first.index, positions.last.index);
-          bottomIndex.value = max(positions.first.index, positions.last.index);
+      onItemPositionEvent() {
+        (switch (scrollableListener.event.value) {
+          ScrollableListEventItemPositions(:final positions) => () {
+              if (positions.isNotEmpty && trivia.isNotEmpty) {
+                topIndex.value = min(positions.first.index, positions.last.index);
+                bottomIndex.value = max(positions.first.index, positions.last.index);
 
-          final top = positions.first.index <= positions.last.index ? positions.first : positions.last;
-          scrollIndex.value = top.index;
-          leadingEdge.value = top.itemLeadingEdge;
-          print("scrollIndex: ${scrollIndex.value}, leadingEdge: ${leadingEdge.value}");
+                // final bottom = positions.last.index > positions.first.index ? positions.last : positions.first;
+                // final leading = max(bottom.itemLeadingEdge, 0.0);
+                // final trailing = min(bottom.itemTrailingEdge, 1.0);
+                // final percent = ((trailing - leading) * 100).toInt();
+                // print("bottom itemは画面表示領域の$percent%を占有しています。");
 
-          // final bottom = positions.last.index > positions.first.index ? positions.last : positions.first;
-          // final leading = max(bottom.itemLeadingEdge, 0.0);
-          // final trailing = min(bottom.itemTrailingEdge, 1.0);
-          // final percent = ((trailing - leading) * 100).toInt();
-          // print("bottom itemは画面表示領域の$percent%を占有しています。");
+                final shouldSmallerFetch = topIndex.value <= 3 && initialized.value;
+                if (shouldSmallerFetch) triviaNotifier.fetchSmaller(10);
 
-          final shouldSmallerFetch = topIndex.value <= 3 && initialized.value;
-          if (shouldSmallerFetch) triviaNotifier.fetchSmaller(10);
-
-          final shouldBiggerFetch = (trivia.length - bottomIndex.value) <= 3 && initialized.value;
-          if (shouldBiggerFetch) triviaNotifier.fetchBigger(10);
-        }
+                final shouldBiggerFetch = (trivia.length - bottomIndex.value) <= 3 && initialized.value;
+                if (shouldBiggerFetch) triviaNotifier.fetchBigger(10);
+              }
+            },
+          _ => () {},
+        })();
       }
 
-      itemPositionsListener.itemPositions.addListener(listener);
-      return () => itemPositionsListener.itemPositions.removeListener(listener);
+      scrollableListener.event.addListener(onItemPositionEvent);
+      return () => scrollableListener.event.removeListener(onItemPositionEvent);
     }, [trivia, initialized]);
-
-    useEffect(() {
-      scrollOffsetListener.changes.listen((event) {
-        scrollHistory.value = [(event, DateTime.now()), ...scrollHistory.value].take(10).toList();
-      });
-      return null;
-    }, []);
-
-    useEffect(() {
-      fuga() {
-        print(listener.event.value);
-      }
-
-      listener.event.addListener(fuga);
-      return () => listener.event.removeListener(fuga);
-    }, []);
 
     useEffect(() {
       const startingNumber = 40;
@@ -106,12 +81,7 @@ class MyHomePage extends HookConsumerWidget {
       WidgetsBinding.instance.addPostFrameCallback((_) async {
         await triviaNotifier.fetchTrivias(List.generate(20, (index) => index + startingNumber));
         WidgetsBinding.instance.addPostFrameCallback((_) async {
-          // 10 + 1番目の要素つまりは#50にスクロール
-          // itemScrollController.scrollTo(
-          // itemScrollController.jumpTo(index: 10 + 1, alignment: 1.0);
-          WidgetsBinding.instance.addPostFrameCallback((_) async {
-            initialized.value = true;
-          });
+          initialized.value = true;
         });
       });
       return null;
@@ -151,10 +121,8 @@ class MyHomePage extends HookConsumerWidget {
                   };
                 },
                 itemScrollController: itemScrollController,
-                itemPositionsListener: itemPositionsListener,
                 scrollOffsetController: scrollOffsetController,
-                scrollOffsetListener: scrollOffsetListener,
-                listener: listener,
+                listener: scrollableListener,
                 initialScrollIndex: initialScrollIndex,
                 initialAlignment: initialLeadingEdge,
                 physics: const RangeMaintainingScrollPhysics(), // NeverScrollableScrollPhysicsにするとユーザーのスクロールを無効にできる
@@ -169,6 +137,9 @@ class MyHomePage extends HookConsumerWidget {
             duration: const Duration(seconds: 2),
             curve: Curves.easeOut,
           );
+          // 10 + 1番目の要素つまりは#50にスクロール
+          // itemScrollController.scrollTo(
+          // itemScrollController.jumpTo(index: 10 + 1, alignment: 1.0);
         },
         tooltip: 'Test',
         child: const Icon(Icons.add),
--- lib/widget/scrollable_list.dart
+++ lib/widget/scrollable_list.dart
@@ -1,11 +1,10 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
+/// ScrollablePositionedListのラッパークラス
 class ScrollableList extends StatefulWidget {
   /// Number of items the [itemBuilder] can produce.
   final int itemCount;
@@ -17,14 +16,10 @@ class ScrollableList extends StatefulWidget {
   /// Controller for jumping or scrolling to an item.
   final ItemScrollController? itemScrollController;
 
-  /// Notifier that reports the items laid out in the list after each frame.
-  final ItemPositionsListener? itemPositionsListener;
-
+  /// Controller for animate to an offset.
   final ScrollOffsetController? scrollOffsetController;
 
-  /// Notifier that reports the changes to the scroll offset.
-  final ScrollOffsetListener? scrollOffsetListener;
-
+  /// ScrollableListEventを受け取れリスナー
   final ScrollableListEventNotifier? notifier;
 
   /// Index of an item to initially align within the viewport.
@@ -49,59 +44,107 @@ class ScrollableList extends StatefulWidget {
     required this.itemCount,
     required this.itemBuilder,
     this.itemScrollController,
-    this.itemPositionsListener,
     this.scrollOffsetController,
-    this.scrollOffsetListener,
     ScrollableListEventListener? listener,
     this.initialScrollIndex = 0,
     this.initialAlignment = 0,
     this.physics,
   }) : notifier = listener as ScrollableListEventNotifier?;
 
-  static StreamController<int> eventStreamController() => StreamController<int>();
-
   @override
-  _ScrollableListState createState() => _ScrollableListState();
+  ScrollableListState createState() => ScrollableListState();
 }
 
-class _ScrollableListState extends State<ScrollableList> {
+class ScrollableListState extends State<ScrollableList> {
   late ItemScrollController? _itemScrollController;
   late ScrollOffsetController? _scrollOffsetController;
-  final controller = StreamController();
+  final _itemPositionsListener = ItemPositionsListener.create();
+  final _scrollOffsetListener = ScrollOffsetListener.create();
+  late StreamSubscription<double>? _subscription;
+  var scrollHistory = (<(double, DateTime)>[]);
 
   @override
   void initState() {
     super.initState();
+    // ScrollableListで指定されたControllerがない場合は自身でControllerを生成する
     _itemScrollController = widget.itemScrollController ?? ItemScrollController();
     _scrollOffsetController = widget.scrollOffsetController ?? ScrollOffsetController();
+    // 内部のリスナーを登録する
     widget.notifier?.event.addListener(_update);
-    print("ScrollableList is mounted.");
+    _itemPositionsListener.itemPositions.addListener(_listenItemPositions);
+    // スクロールの変化を日時とのセットの最新10件を保持する
+    _subscription = _scrollOffsetListener.changes.listen((offset) {
+      scrollHistory = [(offset, DateTime.now()), ...scrollHistory].take(10).toList();
+      // ScrollableListのリスナーにイベントを発行する
+      widget.notifier?.event.value = ScrollableListEventScrollOffset(offset);
+    });
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    print("ScrollableList get ready.");
   }
 
   @override
   void didUpdateWidget(covariant ScrollableList oldWidget) {
     super.didUpdateWidget(oldWidget);
-    print("ScrollableList is updated. $oldWidget -> $widget");
     _itemScrollController = widget.itemScrollController ?? ItemScrollController();
     _scrollOffsetController = widget.scrollOffsetController ?? ScrollOffsetController();
 
-    widget.notifier?.event.value = ScrollableListEventNothing();
+    // アイテムが増えた時の処理
+    final addedCount = widget.itemCount - oldWidget.itemCount;
+    if (addedCount > 0) {
+      // 直近1秒間のスクロール量を積分する
+      // NOTE: 負数は上方向にスクロールしたことを示す
+      final scrollOffsetInSecond = scrollHistory
+          .where((record) => DateTime.now().difference(record.$2).inSeconds <= 1)
+          .fold(0.0, (acc, current) => acc + current.$1);
+
+      // 現在のアイテムの位置を取得する
+      final positions = _itemPositionsListener.itemPositions.value;
+      final top = positions.first.index <= positions.last.index ? positions.first : positions.last;
+      final scrollIndex = top.index;
+      final leadingEdge = top.itemLeadingEdge;
+
+      /*
+       * （上["10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]下）という配列があり、画面上表示されているアイテムが["12", "13", "14"]だとする。
+       * 残りの表示が3つ以下に10個追加ロードする場合、index = 2("12")の時に前方から0..9が足されると["2", "3", "4"]になりいきなりスクロール位置が瞬間的に変わったように見えてしまう。
+       * なお下方向のスクロールで配列が後方から追加される場合は問題がない。
+       */
+      if (addedCount > 0 && scrollOffsetInSecond < 0.0) {
+        /*
+         * そこで、スクロール位置が見た目の上で急に変わらないように増えた分だけスクロール位置を移動してユーザーが元々みていたアイテムが画面上に表示されるようにする。
+         */
+        _itemScrollController?.jumpTo(index: scrollIndex + addedCount, alignment: leadingEdge);
+        // jumpToをすると惰性スクロールが止まってしまうため、指で操作して違和感がないようにする
+        _scrollOffsetController?.animateScroll(
+          // NOTE: この値は適当な値であり、scrollOffsetInSecondに相関性のある畳み込み処理で調整するとより自然な惰性スクロールができる
+          offset: -500.0,
+          duration: const Duration(milliseconds: 1000),
+          curve: Curves.easeOut,
+        );
+      }
+    }
   }
 
   @override
   void dispose() {
-    print("ScrollableList is unmounted.");
     widget.notifier?.event.removeListener(_update);
+    _itemPositionsListener.itemPositions.removeListener(_listenItemPositions);
+    _subscription?.cancel();
     super.dispose();
   }
 
   void _update() {}
+
+  void _listenItemPositions() {
+    // 内部のインデックスが変化した時の処理
+    final positions = _itemPositionsListener.itemPositions.value;
+    final top = positions.first.index <= positions.last.index ? positions.first : positions.last;
+    print("scrollIndex: ${top.index}, leadingEdge: ${top.itemLeadingEdge}");
+    // ScrollableListのリスナーにイベントを発行する
+    widget.notifier?.event.value = ScrollableListEventItemPositions(positions);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -109,15 +152,17 @@ class _ScrollableListState extends State<ScrollableList> {
       itemCount: widget.itemCount,
       itemBuilder: widget.itemBuilder,
       itemScrollController: _itemScrollController,
-      itemPositionsListener: widget.itemPositionsListener,
+      itemPositionsListener: _itemPositionsListener,
       scrollOffsetController: _scrollOffsetController,
-      scrollOffsetListener: widget.scrollOffsetListener,
+      scrollOffsetListener: _scrollOffsetListener,
       initialScrollIndex: widget.initialScrollIndex,
+      initialAlignment: widget.initialAlignment,
       physics: widget.physics,
     );
   }
 }
 
+/// ScrollableListEventListenerが受け取れるイベントの宣言
 sealed class ScrollableListEvent {
   const ScrollableListEvent();
 }
@@ -126,6 +171,19 @@ class ScrollableListEventNothing extends ScrollableListEvent {
   const ScrollableListEventNothing();
 }
 
+/// 内部のアイテムの位置が変化した時に[positions]でその配置をイベントとして発行する
+class ScrollableListEventItemPositions extends ScrollableListEvent {
+  final Iterable<ItemPosition> positions;
+  const ScrollableListEventItemPositions(this.positions);
+}
+
+/// スクロールした時の変化量を[offset]として発行する
+class ScrollableListEventScrollOffset extends ScrollableListEvent {
+  final double offset;
+  const ScrollableListEventScrollOffset(this.offset);
+}
+
+/// ScrollableListで発行されるEventのListenerインターフェース
 abstract class ScrollableListEventListener {
   factory ScrollableListEventListener.create() => ScrollableListEventNotifier();
   ValueListenable<ScrollableListEvent> get event;
